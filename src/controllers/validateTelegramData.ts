b387596290c7c12498a2e6f4f352711bedd4a6e2
custerandgeronimo <andrey.evstratenkov@mail.ru>
2024-09-19 22:45:14 +0300

    вывфывфывфывыф

--- src/controllers/validateTelegramData.ts
+++ src/controllers/validateTelegramData.ts
@@ -34,30 +34,26 @@ function validateTelegramData(initData: string): InitData | null {
     return null;
   }
 
-  // Формируем строку проверки данных (data_check_string)
   const dataCheckString = Array.from(params.entries())
-    .filter(([key]) => key !== 'hash')
+    .filter(([key]) => key !== 'hash') // Исключаем хэш
     .map(([key, value]) => `${key}=${value}`)
     .sort()
     .join('\n');
 
-  // Ключ для HMAC-SHA256
   const secretKey = crypto
     .createHmac('sha256', 'WebAppData')
     .update(BOT_TOKEN)
     .digest();
 
-  // Проверяем подпись
   const computedHash = crypto
     .createHmac('sha256', secretKey)
     .update(dataCheckString)
     .digest('hex');
 
   if (computedHash === hash) {
-    // Преобразуем параметры в объект InitData
     const result: InitData = {
       query_id: params.get('query_id') || '',
-      user: JSON.parse(params.get('user') || '{}') as WebAppUser, // Преобразуем строку user в объект WebAppUser
+      user: JSON.parse(decodeURIComponent(params.get('user') || '{}')) as WebAppUser,
       auth_date: parseInt(params.get('auth_date') || '0', 10),
       hash: hash,
       start_params: params.get('start_params') || '',
